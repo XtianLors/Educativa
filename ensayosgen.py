3fcff237a0ac4ec490db6052d476f681f1886aac
CristianDanielLopez <crisdalori@gmail.com>
2025-03-12 10:58:38 -0600

    Estructura del código

---
+++ ensayosgen.py
@@ -0,0 +1,18 @@
+
+
+
+class Contenidos:
+    __init__=""
+
+    def 
+
+Abstract = ""
+Conclusiones = ""
+
+Encabezados = ""
+Contenido = ""
+
+
+with open("hello.txt") as my_file:
+    my_file.write()
+    print(my_file.read())
